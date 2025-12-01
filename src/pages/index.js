38159cf9f707ed08047cdde83d9bd6129fc347ec
Villyan <150153700+villyanfm@users.noreply.github.com>
2025-12-01 07:43:46 -0400

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import { Container, Row } from "react-bootstrap";
+import Cards from "./components/cards";
+export async function getServerSideProps() {
+  // Fetch data from  API notícias
+  const res = await fetch('https://apinoticias6b.vercel.app/noticias')
+  const repo = await res.json()
+  // Pass data to the page via props
+  return { props: { noticias: repo } }
+}
+export default function Home({ noticias }) {
+  return <>
+    <Container>
+      <Row xs={1} md={3}>
+    
+        {Array.isArray(noticias) ?
+          noticias.map(noticia => (
+            <Cards idnoticia={noticia._id}
+              conteudonoticia={noticia.conteudonoticia}
+              titulonoticia={noticia.titulonoticia}
+              datahoracadastro={noticia.datahoracadastro}
+              tiponoticia={noticia.tiponoticia} />
+          ))
+          : "não"}
+      </Row>
+    </Container>
+  </>
+}
