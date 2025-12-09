6412adfc6a1dbcfd13f81680319ba9380bc95b2b
Villyan <150153700+villyanfm@users.noreply.github.com>
2025-12-09 15:08:39 -0400

    link da api

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { Container, Row } from "react-bootstrap";
 import Cards from "./components/cards";
 export async function getServerSideProps() {
   // Fetch data from  API notícias
-  const res = await fetch('https://apinoticias6b.vercel.app/noticias')
+  const res = await fetch('https://apivillyanb.vercel.app/noticias')
   const repo = await res.json()
   // Pass data to the page via props
   return { props: { noticias: repo } }
